bdbc8cb6e5691efb4e65239a9b638c67ec6ab128
kirillka.k@gmail.com <108414175+Kiryaoo@users.noreply.github.com>
2025-05-13 20:32:40 +0300

    Added Numpy library and api endpoint

--- python/spaceship/router/api.py
+++ python/spaceship/router/api.py
@@ -1,8 +1,22 @@
 from fastapi import APIRouter
+import numpy as np
 
 router = APIRouter()
 
 
-@router.get('')
+@router.get("")
 def hello_world() -> dict:
-    return {'msg': 'Hello, World!'}
+    return {"msg": "Hello, World!"}
+
+
+@router.get("/matrices")
+def multiply_matrices() -> dict:
+    matrix_a = np.random.rand(10, 10).tolist()
+    matrix_b = np.random.rand(10, 10).tolist()
+    product = (np.dot(np.array(matrix_a), np.array(matrix_b))).tolist()
+
+    return {
+        "matrix_a": matrix_a,
+        "matrix_b": matrix_b,
+        "product": product
+    }
