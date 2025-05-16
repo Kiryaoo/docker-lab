500b37e5fcd5cdead27d31d873f2969bb66e44fd
kirillka.k@gmail.com <108414175+Kiryaoo@users.noreply.github.com>
2025-05-16 22:57:48 +0300

    Added task 3 for python (smaller basic image)

--- python/spaceship/router/api.py
+++ python/spaceship/router/api.py
@@ -1,22 +1,8 @@
 from fastapi import APIRouter
-import numpy as np
 
 router = APIRouter()
 
 
-@router.get("")
+@router.get('')
 def hello_world() -> dict:
-    return {"msg": "Hello, World!"}
-
-
-@router.get("/matrices")
-def multiply_matrices() -> dict:
-    matrix_a = np.random.rand(10, 10).tolist()
-    matrix_b = np.random.rand(10, 10).tolist()
-    product = (np.dot(np.array(matrix_a), np.array(matrix_b))).tolist()
-
-    return {
-        "matrix_a": matrix_a,
-        "matrix_b": matrix_b,
-        "product": product
-    }
+    return {'msg': 'Hello, World!'}
